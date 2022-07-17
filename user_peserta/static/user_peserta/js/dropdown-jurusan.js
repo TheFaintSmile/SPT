a4c3aee5b2754f4f0123d09ef3fac5f4b7b0846f
radityahype <Rprathama2002@gmail.com>
2022-07-17 16:25:40 +0700

    fix fpsiko dropdown

--- user_peserta/static/user_peserta/js/dropdown-jurusan.js
+++ user_peserta/static/user_peserta/js/dropdown-jurusan.js
@@ -1,5 +1,5 @@
-const jurusan = ["jurusan-fk", "jurusan-fkg", "jurusan-fkm", "jurusan-ff", "jurusan-fik", "jurusan-fmipa", "jurusan-fasilkom", "jurusan-ft", "jurusan-fh", "jurusan-fisip", "jurusan-fib", "jurusan-feb", "jurusan-fia", "jurusan-vokasi"];
-const dictJurusan = {'fk':'jurusan-fk', 'fkg':'jurusan-fkg', 'fkm':'jurusan-fkm', 'ff':'jurusan-ff', 'fik':'jurusan-fik','fmipa':'jurusan-fmipa','fasilkom':'jurusan-fasilkom','ft':'jurusan-ft','fh':'jurusan-fh','fisip':'jurusan-fisip','fib':'jurusan-fib','feb':'jurusan-feb','fia':'jurusan-fia','vokasi':'jurusan-vokasi',}
+const jurusan = ["jurusan-fk", "jurusan-fkg", "jurusan-fkm", "jurusan-ff", "jurusan-fik", "jurusan-fmipa", "jurusan-fasilkom", "jurusan-ft", "jurusan-fh", "jurusan-fpsiko", "jurusan-fisip", "jurusan-fib", "jurusan-feb", "jurusan-fia", "jurusan-vokasi"];
+const dictJurusan = {'fk':'jurusan-fk', 'fkg':'jurusan-fkg', 'fkm':'jurusan-fkm', 'ff':'jurusan-ff', 'fik':'jurusan-fik','fmipa':'jurusan-fmipa','fasilkom':'jurusan-fasilkom','ft':'jurusan-ft','fh':'jurusan-fh','fpsiko':'jurusan-fpsiko','fisip':'jurusan-fisip','fib':'jurusan-fib','feb':'jurusan-feb','fia':'jurusan-fia','vokasi':'jurusan-vokasi',}
 
 function showJurusan(s) {
     for (var i in dictJurusan) {

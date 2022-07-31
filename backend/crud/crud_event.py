195ec6f944f2e9d30296193b7540cf50b2059cdc
Romi Fadhurrohman Nabil <romi.fadhurrohman@gmail.com>
2022-07-31 21:28:04 +0700

    fix frontend for dashboard page

---
+++ backend/crud/crud_event.py
@@ -0,0 +1,12 @@
+import firebase_admin
+from firebase_admin import credentials, firestore, storage, auth
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate("spt-bemui-privateKey.json")
+    firebase_admin.initialize_app(cred, {
+    'storageBucket' : 'spt-bemui-2add1.appspot.com'
+    })
+
+db = firestore.client()
+ds = storage.bucket()
+

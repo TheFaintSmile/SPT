fab134c98b996f2b3d26cef75115b6a94d3d8172
Romi Fadhurrohman Nabil <romi.fadhurrohman@gmail.com>
2022-09-05 20:32:24 +0700

    add crud for event kepanitiaan

--- backend/crud/crud_event.py
+++ backend/crud/crud_event.py
@@ -10,3 +10,32 @@ if not firebase_admin._apps:
 db = firestore.client()
 ds = storage.bucket()
 
+def event_create(nama, email,deskripsi, tugas, tanggal, divisi) :
+    
+    data = {
+        'nama' : nama,
+        'email' : email,
+        'deskripsi' : deskripsi,
+        'tugas' : tugas,
+        'tanggal' : tanggal,
+        'divisi' : divisi
+    }
+    db.collection('events').document(email).set(data)
+
+def event_read(emailEvent) :
+    data = db.collection('events').document(emailEvent).get().to_dict()
+    print('Successfully fetched user data: {0}'.format(data))
+    return data
+
+def event_update(emailEvent, nama, deskripsi, tugas, tanggal, divisi) :
+    data = {
+        'nama' : nama,
+        'deskripsi' : deskripsi,
+        'tugas' : tugas,
+        'tanggal' : tanggal,
+        'divisi' : divisi
+    }
+    db.collection('events').document(emailEvent).set(data)
+
+def event_delete(emailEvent) :
+    db.collection('events').document(emailEvent).delete()

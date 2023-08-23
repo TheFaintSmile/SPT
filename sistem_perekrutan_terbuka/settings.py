584acc67df85bcccdd598232d5f23598df2ed2f6
Alfredo <alfredo.austin@ui.ac.id>
2023-08-24 00:22:10 +0700

    fix: remove pycryptodome

--- sistem_perekrutan_terbuka/settings.py
+++ sistem_perekrutan_terbuka/settings.py
@@ -32,7 +32,7 @@ PRODUCTION = os.getenv('DATABASE_URL') is not None
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".domcloud.io", "sptbemui.com", "sptbemuibackend.com"]
+ALLOWED_HOSTS = ["sptbackend.domcloud.io"]
 
 if not PRODUCTION:
     ALLOWED_HOSTS += ['.localhost', '127.0.0.1', '[::1]']
